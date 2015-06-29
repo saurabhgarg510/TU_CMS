a2292cb8ed73c5a1e9945cb9fad33343895a4ce2
virk-iman <virk.iman@gmail.com>
2015-06-29 21:08:46 +0530

    new chart with exploded feature

--- application/views/admin/poll_result.php
+++ application/views/admin/poll_result.php
@@ -26,6 +26,36 @@
 
 <script defer>
     function show(q, o1, o2, o3, o4, c1, c2, c3, c4) {
+	var m=Math.max(c1,c2,c3,c4);
+	var temp;
+	if(c2===m)
+	{
+	temp=c1;
+	c1=c2;
+	c2=temp;
+	temp=o1;
+	o1=o2;
+	o2=temp;
+	}
+	if(c3===m)
+	{
+	temp=c1;
+	c1=c3;
+	c3=temp;
+	temp=o1;
+	o1=o3;
+	o3=temp;
+	}
+	if(c4===m)
+	{
+	temp=c1;
+	c1=c4;
+	c4=temp;
+	temp=o1;
+	o1=o4;
+	o4=temp;
+	}
+	
         var chart = new CanvasJS.Chart("chartContainer",
                 {
                     title: {
@@ -47,8 +77,8 @@
                             showInLegend: true,
                             toolTipContent: "{y} - <strong>#percent%</strong>",
                             dataPoints: [
-                                {y: c1, legendText: o1, indexLabel: o1},
-                                {y: c2, legendText: o2, indexLabel: o2},
+                                {y: c1, legendText: o1,exploded:true, indexLabel: o1},
+                                {y: c2, legendText: o2,indexLabel: o2},
                                 {y: c3, legendText: o3, indexLabel: o3},
                                 {y: c4, legendText: o4, indexLabel: o4}
                             ]
@@ -56,6 +86,7 @@
                     ]
                 });
         chart.render();
+		
     }
 </script>
 <script type="text/javascript" src="<?php echo base_url(); ?>public/js/canvasjs.min.js" defer></script>
